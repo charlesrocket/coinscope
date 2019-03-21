1e75e052c6c8f2ccee5bcb2ab69e9e242642f641
Sergi Delgado <sergi.delgado.s@gmail.com>
2019-03-21 17:00:13 +0000

    Updates lib with a more recent version of the code. Includes Surya's conntols.

--- libraries/python/lib/common.py
+++ libraries/python/lib/common.py
@@ -1,17 +1,20 @@
 from datetime import *
 from time import time
 
+
 def inet_ntoa(num):
     # python version wants the num to be packed
-    return str(num & 0xff) + '.' + str((num >> 8) & 0xff) + '.' + str((num >> 16) & 0xff) + '.' + str((num >> 24) & 0xff)
+    return str(num & 0xff) + '.' + str((num >> 8) & 0xff) + '.' + str((num >> 16) & 0xff) + '.' + \
+           str((num >> 24) & 0xff)
+
 
 def inet_aton(ip):
     i = map(int, ip.split('.'))
     return (i[3] << 24) | (i[2] << 16) | (i[1] << 8) | i[0]
+
 
 def unix2str(tstamp):
     if tstamp > time():
         tstamp = tstamp / 1E6
     dt = datetime.fromtimestamp(tstamp)
     return dt.isoformat(' ')
-    
