95cc0dee845da5244907ef38cb00692f180c9233
Xu Wang <wilyxerus@gmail.com>
2020-01-03 14:39:30 +0800

    fix build bugs

--- shared/includes/autogen.hpp
+++ shared/includes/autogen.hpp
@@ -1,6 +1,6 @@
 #ifndef AUTOGEN_HPP
 #define AUTOGEN_HPP
 
-const char commit_hash[] = "8f39fbfbd4751f385075322f985dbaa66bde1163";
+const char commit_hash[] = "e853726d4bd3273503f91ce784a3f4140840dee0";
 
 #endif
